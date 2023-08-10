34345a0074d85135794d203400fb76c089163516
Ruslan Riznyk <ruslan_riznyk@epam.com>
2023-08-10 15:04:53 +0300

    feat: refactored code 	modified:   main.tf 	modified:   variables.tf 	modified:   versions.tf

--- main.tf
+++ main.tf
@@ -1,69 +1,158 @@
-resource "google_service_account" "bastion_sa" {
-  account_id   = "${var.env}-${var.product_base_name}-bastion-sa"
-  display_name = "${var.env}-${var.product_base_name}-bastion-sa"
+locals {
+  prefix = length(var.prefix) == 0 ? "" : "${var.prefix}-"
+  suffix = length(var.suffix) == 0 ? "" : "-${var.suffix}"
+  env    = length(var.env) == 0 ? "" : "${var.env}-"
+  name   = lower("${local.prefix}${local.env}vm${local.suffix}")
 }
 
-resource "google_project_iam_member" "bastion" {
-  project  = var.project_id
-  for_each = var.bastion_roles
-  role     = each.key
-  member   = "serviceAccount:${google_service_account.bastion_sa.email}"
+resource "google_service_account" "compute_vm" {
+  count = alltrue([var.service_account.email != null, length(var.service_account.roles) == 0]) ? 1 : 0
+
+  account_id   = "${local.name}-sa"
+  display_name = "${local.name}-sa"
 }
 
+resource "google_project_iam_member" "compute_vm" {
+  for_each = length(var.service_account.roles) != 0 ? var.service_account.roles : []
 
-## VM instace section
+  project = var.project_id
+  role    = each.key
+  member  = "serviceAccount:${google_service_account.compute_vm[0].email}"
+}
+
 resource "google_storage_bucket_object" "startup_scripts" {
-  for_each = toset(var.scripts)
-  name     = "vm_templates/${each.key}"
+  for_each = var.source_bucket != null ? toset(var.scripts) : []
+  name     = "compute_vm_scripts/${each.key}"
   bucket   = var.source_bucket
   content  = templatefile("../scripts/${each.key}", var.scripts_vars)
-
-  depends_on = [google_project_iam_member.bastion]
 }
 
-resource "google_compute_instance" "bastion" {
-  name           = "${var.env}-${var.product_base_name}-vm-instance"
-  machine_type   = var.machine_type
-  can_ip_forward = var.ip_forward
-  metadata = {
-    startup-script-url = var.startup_script_url
-  }
-  boot_disk {
-    initialize_params {
-      type  = "pd-ssd"
-      size  = var.disk_size
-      image = var.image
-    }
-  }
-  network_interface {
-    network = var.network
-    access_config {
-      #    Ephemeral public IP
-    }
-    subnetwork = var.subnet
-  }
-  service_account {
-    email  = google_service_account.bastion_sa.email
-    scopes = var.scopes
-  }
-  allow_stopping_for_update = true
+resource "google_compute_instance" "vm" {
+  name                      = local.name
+  machine_type              = var.machine_type
+  zone                      = var.zone
+  allow_stopping_for_update = var.allow_stopping_for_update
+  can_ip_forward            = var.can_ip_forward
+  description               = var.description
+  desired_status            = var.desired_status
+  deletion_protection       = var.deletion_protection
+  hostname                  = var.hostname
+  metadata = merge(var.metadata, var.startup_script != null ? {
+    "startup-script-url" = try(regex("^((gs|http)://).+", var.startup_script) != null ? var.startup_script : google_storage_bucket_object.startup_scripts[var.startup_script] != null ? google_storage_bucket_object.startup_scripts[var.startup_script].self_link : null, null)
+  } : {})
+  metadata_startup_script = var.metadata_startup_script
+  min_cpu_platform        = var.min_cpu_platform
 
+  dynamic "boot_disk" {
+    for_each = var.boot_disk != null ? [var.boot_disk] : []
+    content {
+      auto_delete             = lookup(boot_disk.value, "auto_delete", true) # Set the default value for auto_delete to true
+      device_name             = lookup(boot_disk.value, "device_name", null)
+      mode                    = lookup(boot_disk.value, "mode", null)
+      disk_encryption_key_raw = lookup(boot_disk.value, "disk_encryption_key_raw", null)
+      kms_key_self_link       = lookup(boot_disk.value, "kms_key_self_link", null)
+      source                  = lookup(boot_disk.value, "source", null)
+      dynamic "initialize_params" {
+        for_each = boot_disk.value.initialize_params != null ? [boot_disk.value.initialize_params] : []
+        content {
+          size                  = lookup(initialize_params.value, "size", null)
+          type                  = lookup(initialize_params.value, "type", null)
+          image                 = lookup(initialize_params.value, "image", null)
+          labels                = lookup(initialize_params.value, "labels", null)
+          resource_manager_tags = lookup(initialize_params.value, "resource_manager_tags", null)
+        }
+      }
+    }
+  }
+
+  dynamic "network_interface" {
+    for_each = var.network_interfaces
+    content {
+      network            = lookup(network_interface.value, "network", null)
+      subnetwork         = lookup(network_interface.value, "subnetwork", null)
+      subnetwork_project = lookup(network_interface.value, "subnetwork_project", null)
+      network_ip         = lookup(network_interface.value, "network_ip", null)
+
+      dynamic "access_config" {
+        for_each = network_interface.value.access_config != null ? [network_interface.value.access_config.external_ip ? {
+        } : network_interface.value.access_config] : []
+
+        content {
+          nat_ip                 = lookup(access_config.value, "nat_ip", null)
+          public_ptr_domain_name = lookup(access_config.value, "public_ptr_domain_name", null)
+          network_tier           = lookup(access_config.value, "network_tier", null)
+        }
+      }
+      dynamic "alias_ip_range" {
+        for_each = network_interface.value.alias_ip_range != null ? [network_interface.value.alias_ip_range] : []
+        content {
+          ip_cidr_range         = lookup(alias_ip_range.value, "", null)
+          subnetwork_range_name = lookup(alias_ip_range.value, "", null)
+        }
+      }
+
+      nic_type   = lookup(network_interface.value, "nic_type", null)
+      stack_type = lookup(network_interface.value, "stack_type", null)
+      dynamic "ipv6_access_config" {
+        for_each = network_interface.value.ipv6_access_config != null ? [network_interface.value.ipv6_access_config.external_ip ? {
+        } : network_interface.value.ipv6_access_config] : []
+        content {
+          public_ptr_domain_name = lookup(ipv6_access_config.value, "public_ptr_domain_name", null)
+          network_tier           = lookup(ipv6_access_config.value, "network_tier", null)
+        }
+      }
+
+      queue_count = lookup(network_interface.value, "queue_count", null)
+    }
+  }
+
+  dynamic "attached_disk" {
+    for_each = var.attached_disk != null ? [var.attached_disk] : []
+    content {
+      source                  = lookup(attached_disk.value, "source", null)
+      device_name             = lookup(attached_disk.value, "device_name", null)
+      mode                    = lookup(attached_disk.value, "mode", null)
+      disk_encryption_key_raw = lookup(attached_disk.value, "disk_encryption_key_raw", null)
+      kms_key_self_link       = lookup(attached_disk.value, "kms_key_self_link", null)
+    }
+  }
+
+  dynamic "guest_accelerator" {
+    for_each = var.guest_accelerator != null ? [var.guest_accelerator] : []
+    content {
+      type  = lookup(guest_accelerator.value, "source", null)
+      count = lookup(guest_accelerator.value, "source", null)
+    }
+  }
+
+  dynamic "params" {
+    for_each = var.resource_manager_tags != null ? [var.resource_manager_tags] : []
+    content {
+      resource_manager_tags = params.value
+    }
+  }
+
+  dynamic "service_account" {
+    for_each = var.service_account != null ? [var.service_account] : []
+    content {
+      email  = length(var.service_account.roles) != 0 ? google_service_account.compute_vm[0].email : lookup(service_account.value, "email", null)
+      scopes = lookup(service_account.value, "scopes", null)
+    }
+  }
+
+  tags   = var.tags
   labels = var.labels
 
-  depends_on = [google_storage_bucket_object.startup_scripts]
+  # scheduling=
+  # scratch_disk=
+  # shielded_instance_config=
+  # enable_display=
+  # resource_policies=
+  # reservation_affinity=
+  # confidential_instance_config=
+  # advanced_machine_features=
+  # network_performance_config=
 }
-
-## Firewall (SSH) section
-resource "google_compute_firewall" "ssh_rule" {
-  project  = var.project_id
-  name     = "${var.env}-${var.product_base_name}-gcpconsol-ingress-allow"
-  network  = var.network
-  priority = "100"
-  allow {
-    protocol = "tcp"
-    ports    = var.rem_conn_port
-  }
-  direction     = "INGRESS"
-  source_ranges = var.remote_from
-  description   = "Allow remote connection from GCP consol to the bastion instace"
-}
+# output "access_config" {
+#   value = [for test in var.network_interfaces : test.access_config]
+# }
--- variables.tf
+++ variables.tf
@@ -1,62 +1,225 @@
 variable "project_id" {
-  description = "ID of the project in which the resources should be created."
+  description = "The ID of the project in which to create the instance."
   type        = string
 }
 
-variable "product_base_name" {
-  description = "Cloud resources base name (used to create services)"
+variable "region" {
+  description = "The region in which to create the instance."
   type        = string
+  default     = null
+}
+
+variable "zone" {
+  description = "The zone in which to create the instance."
+  type        = string
+  default     = null
 }
 
 variable "env" {
-  description = "Variable to mark the environment of the resource (used to create services)."
+  description = "Variable to mark the environment of the resources (used to create resources names)"
   type        = string
+  default     = null
 }
 
-variable "disk_size" {
-  description = "The size of the image (in gigabytes) for the boot disk."
+variable "prefix" {
+  description = "Prefix for resource name"
   type        = string
-  default     = "10"
+  default     = null
 }
 
-variable "image" {
-  description = "The image from which to initialize the boot disk for the instance."
-  type        = string
-  default     = "ubuntu-2004-focal-v20220927"
+variable "suffix" {
+  description = "Suffix for resource name"
+  default     = null
 }
 
-variable "network" {
-  description = "Networks to attach to the instance."
+
+# variable "instance_name" {
+#   description = "The name to assign to the instance."
+#   type        = string
+# }
+
+variable "machine_type" {
+  description = "The machine type to use for the instance (e.g. 'n1-standard-1')."
   type        = string
+  default     = "e2-small"
 }
 
-variable "subnet" {
-  description = "The Compute Engine subnetwork to be used for machine communications"
-  type        = string
+variable "boot_disk" {
+  description = "The configuration for the boot disk."
+  type = object({
+    auto_delete             = optional(bool)
+    device_name             = optional(string)
+    mode                    = optional(string)
+    disk_encryption_key_raw = optional(string)
+    kms_key_self_link       = optional(string)
+    source                  = optional(string)
+    initialize_params = optional(object({
+      size                  = optional(number)
+      type                  = optional(string)
+      image                 = optional(string)
+      labels                = optional(map(string))
+      resource_manager_tags = optional(map(string))
+    }))
+  })
+  default = {
+    auto_delete = true
+    initialize_params = {
+      size  = 10
+      type  = "pd-ssd"
+      image = "ubuntu-2204-jammy-v20230727"
+    }
+  }
 }
 
-variable "bastion_roles" {
-  description = "Roles that should be applied for Bastion service account."
+variable "network_interfaces" {
+  description = "The list of network interfaces to attach to the instance."
+  type = list(object({
+    network            = optional(string)
+    subnetwork         = optional(string)
+    subnetwork_project = optional(string)
+    network_ip         = optional(string)
+
+    access_config = optional(object({
+      external_ip            = optional(bool, false)
+      nat_ip                 = optional(string, null)
+      public_ptr_domain_name = optional(string, null)
+      network_tier           = optional(string, null)
+    }))
+
+    alias_ip_range = optional(list(object({
+      ip_cidr_range         = optional(string)
+      subnetwork_range_name = optional(string)
+    })))
+
+    nic_type   = optional(string)
+    stack_type = optional(string)
+
+    ipv6_access_config = optional(list(object({
+      enabled                = optional(bool, false)
+      public_ptr_domain_name = optional(string, null)
+      network_tier           = optional(string, null)
+    })))
+
+    queue_count = optional(number)
+  }))
+
+  default = null
+}
+
+variable "allow_stopping_for_update" {
+  description = "Whether to allow the instance to be stopped temporarily during updates."
+  type        = bool
+  default     = true
+}
+
+variable "attached_disk" {
+  description = "Additional disks to attach to the instance. Can be repeated multiple times for multiple disks"
+  type = object({
+    source                  = string
+    device_name             = optional(string)
+    mode                    = optional(string)
+    disk_encryption_key_raw = optional(string)
+    kms_key_self_link       = optional(string)
+  })
+  default = null
+}
+
+variable "can_ip_forward" {
+  description = "Whether to allow sending and receiving of packets with non-matching source or destination IPs"
+}
+
+variable "description" {
+  description = "A brief description of this resource"
+  type        = string
+  default     = null
+}
+
+variable "desired_status" {
+  description = "Desired status of the instance. Either RUNNING or TERMINATED."
+  type        = string
+  default     = null
+}
+
+variable "deletion_protection" {
+  description = "Enable deletion protection on this instance."
+  type        = bool
+  default     = false
+}
+
+variable "hostname" {
+  description = "A custom hostname for the instance."
+  type        = string
+  default     = null
+}
+
+variable "guest_accelerator" {
+  description = "List of the type and count of accelerator cards attached to the instance."
+  type = object({
+    type  = string
+    count = number
+  })
+  default = null
+}
+
+variable "labels" {
+  description = "A map of key/value label pairs to assign to the instance."
+  type        = map(string)
+  default     = null
+}
+
+variable "metadata" {
+  description = "Additional metadata to pass to the instance."
+  type        = map(string)
+  default     = null
+}
+
+variable "metadata_startup_script" {
+  description = <<-EOT
+An alternative to using the startup-script metadata key, except this one forces the instance to be recreated (thus re-running the script) if it is changed. This replaces the startup-script metadata key on the created instance and thus the two mechanisms are not allowed to be used simultaneously.
+EOT
+  type        = string
+  default     = null
+}
+
+variable "scripts" {
+  description = "Names of the template files of the scripts (location for templates should be: ../scripts/ )"
   type        = set(string)
+  default     = []
+}
+
+
+variable "min_cpu_platform" {
+  description = "Specifies a minimum CPU platform for the VM instance."
+  type        = string
+  default     = null
+}
+
+variable "resource_manager_tags" {
+  description = "Additional instance parameters."
+  type        = object({})
+  default     = null
+}
+
+variable "service_account" {
+  description = "The email address of the service account to grant access to the instance."
+  type = object({
+    email  = optional(string)
+    roles  = optional(set(string), [])
+    scopes = optional(list(string), ["https://www.googleapis.com/auth/cloud-platform"])
+  })
+  default = {}
+}
+
+variable "tags" {
+  description = "The list of tags to apply to the instance."
+  type        = list(string)
   default     = []
 }
 
 variable "source_bucket" {
   description = "GCP Storage Bucket for storing scripts and temporary data."
   type        = string
-  default     = ""
-}
-
-variable "startup_script_url" {
-  description = "The URL of the startup script located on the GCP Cloud Storage Bucket."
-  type        = string
-  default     = ""
-}
-
-variable "scripts" {
-  description = "Names of the template files of the scripts (location for templates should be: ./templates/ )"
-  type        = set(string)
-  default     = []
+  default     = null
 }
 
 variable "scripts_vars" {
@@ -65,38 +228,33 @@ variable "scripts_vars" {
   default     = {}
 }
 
-variable "machine_type" {
-  description = "The machine type to create"
+variable "startup_script" {
+  description = "The URL of the startup script located on the GCP Cloud Storage Bucket."
   type        = string
-  default     = "e2-small"
+  default     = null
 }
 
-variable "ip_forward" {
-  description = "Whether to allow sending and receiving of packets with non-matching source or destination IPs"
-  type        = bool
-  default     = false
-}
 
-variable "scopes" {
-  description = "A list of service scopes for VM."
-  type        = set(string)
-  default     = ["https://www.googleapis.com/auth/cloud-platform"]
-}
+# variable "scheduling" {
+#   description = "The scheduling strategy to use."
+#   type = object({
+#     preemptible=optional(bool)
+# on_host_maintenance=optional(string)
+# automatic_restart=
+# node_affinities=
+# min_node_cpus=
+# provisioning_model=
+# instance_termination_action=
+# max_run_duration=
+# nanos=
+# seconds=
+# maintenance_interval=
+# type=
+# count=
+#   })
+# }
 
-variable "labels" {
-  description = "The labels associated with this module."
-  type        = map(string)
-  default     = {}
-}
-
-variable "remote_from" {
-  description = "Allow remote connection to bastion instace from provided subnet CIDR range. For GCP consol provide 35.235.240.0/20"
-  type        = set(string)
-  default     = ["35.235.240.0/20"]
-}
-
-variable "rem_conn_port" {
-  description = "Allow remote connection to bastion instace through provided port."
-  type        = set(string)
-  default     = ["22"]
-}
+# variable "scratch_disk" {
+#   description = "Scratch disks to attach to the instance."
+#   # type=
+# }
